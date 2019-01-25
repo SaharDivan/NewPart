94791acdb637d4d25fb89d51feb20b6c560aed8a
SaharDivan <saharsahebdivan@gmail.com>
2019-01-25 10:10:51 +0100

    FilmSummerizer

--- week10Stream/week10Reflection/BookStaticApplication.java
+++ week10Stream/week10Reflection/BookStaticApplication.java
@@ -1,24 +1,22 @@
 package week10Stream.week10Reflection;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
-import static java.util.Comparator.*;
-import static java.util.stream.Collectors.*;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class BookStaticApplication {
     public static void main(String[] args) {
 
 
         BookReader bookReader = new BookReader();
-        List<String> bookWords=bookReader.cleanTheText();
+        List<String> bookWords = bookReader.cleanTheText();
 
         long numberOfWords = bookWords.stream()
                 .count();
-        System.out.println("Total number of Words | "+ numberOfWords);
+        System.out.println("Total number of Words | " + numberOfWords);
 
         long numberOfDistinctWords = bookWords.stream()
                 .distinct()
@@ -26,19 +24,23 @@ public class BookStaticApplication {
         System.out.println("Total number of distinct words | " + numberOfDistinctWords);
 
 
-                bookWords.stream()
+        bookWords.stream()
                 .distinct()
                 .collect(groupingBy(String::length))
                 .entrySet().stream()
                 .sorted(comparing(Map.Entry::getKey))
                 .limit(1)
-                .forEach(e-> System.out.println(e.getValue()));
+                .forEach(e -> System.out.println(e.getValue()));
 
-         bookWords.stream()
+        bookWords.stream()
+                .map(e -> e.trim())
                 .collect(groupingBy(e -> e, counting()))
                 .entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
                 .limit(5)
-                .forEach(e-> System.out.println(e.getValue()+e.getValue()));
+                .forEach(e -> System.out.println(e.getKey() + " | " + e.getValue()));
+
+
 
     }
 
